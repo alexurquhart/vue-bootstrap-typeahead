cad26cfa52c7c5da8bfe6f427f56833cd4f56332
Alex Urquhart <alexurquhart@gmail.com>
2018-08-29 08:27:07 -0700

    Working on IE compatibility

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
-
 import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'whatwg-fetch'
 
 Vue.config.productionTip = false
 
