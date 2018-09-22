e25a57210ed2403f6e69536cb11c3444a7990937
cdn-madness <alexurquhart@gmail.com>
2018-09-22 09:59:31 -0700

    new docs build

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import vueHljs from 'vue-hljs'
+import VueGtm from 'vue-gtm'
 import 'highlight.js/styles/color-brewer.css'
 import 'whatwg-fetch'
 
@@ -11,6 +12,12 @@ Vue.use(vueHljs)
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+
+Vue.use(VueGtm, {
+  id: 'UA-29172866-5',
+  enabled: process.env.NODE_ENV === 'production',
+  vueRouter: router
+})
 
 new Vue({
   router,
