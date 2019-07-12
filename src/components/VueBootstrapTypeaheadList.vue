c9609ece52c419eea48684ad07a28d47207d56ba
Tim Schipper <tim@aranea-hosting.nl>
2019-07-12 15:14:05 +0200

    Keyboard navigation

--- src/components/VueBootstrapTypeaheadList.vue
+++ src/components/VueBootstrapTypeaheadList.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="list-group shadow">
+  <div class="list-group shadow" ref="suggestionList">
     <vue-bootstrap-typeahead-list-item
       v-for="(item, id) in matchedItems" :key="id"
+      :active="isListItemActive(id)"
       :data="item.data"
       :html-text="highlight(item.text)"
       :background-variant="backgroundVariant"
@@ -59,6 +60,12 @@ export default {
     }
   },
 
+  data() {
+    return {
+      activeListItem: -1
+    }
+  },
+
   computed: {
     highlight() {
       return (text) => {
@@ -97,10 +104,62 @@ export default {
     }
   },
 
+  created () {
+    this.$parent.$on('input', this.resetActiveListItem)
+    this.$parent.$on('keyup.down', this.selectNextListItem)
+    this.$parent.$on('keyup.up', this.selectPreviousListItem)
+    this.$parent.$on('keyup.enter', this.hitActiveListItem)
+  },
+
+  watch: {
+    activeListItem (newValue, oldValue) {
+      if (newValue >= 0) {
+        const scrollContainer = this.$refs.suggestionList
+        const listItem = scrollContainer.children[this.activeListItem]
+        const scrollContainerlHeight = scrollContainer.clientHeight
+        const listItemHeight = listItem.clientHeight
+        const visibleItems = Math.floor(
+          scrollContainerlHeight / listItemHeight
+        )
+        if (newValue >= visibleItems) {
+          scrollContainer.scrollTop = listItemHeight * this.activeListItem
+        } else {
+          scrollContainer.scrollTop = 0
+        }
+      }
+    }
+  },
+
   methods: {
     handleHit(item, evt) {
       this.$emit('hit', item)
       evt.preventDefault()
+    },
+    isListItemActive(id) {
+      return this.activeListItem === id
+    },
+    resetActiveListItem() {
+      this.activeListItem = -1
+    },
+    selectNextListItem() {
+      if (this.activeListItem < this.matchedItems.length - 1) {
+        this.activeListItem++
+      } else {
+        this.activeListItem = -1
+      }
+    },
+    selectPreviousListItem() {
+      if (this.activeListItem < 0) {
+        this.activeListItem = this.matchedItems.length - 1
+      } else {
+        this.activeListItem--
+      }
+    },
+    hitActiveListItem() {
+      if (this.activeListItem >= 0) {
+        this.$emit('hit', this.matchedItems[this.activeListItem])
+        this.activeListItem--
+      }
     }
   }
 }
