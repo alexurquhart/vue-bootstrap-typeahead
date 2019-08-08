5c5833525be2394feed84f87af5c652ba2a6b702
Broxson, Mitchell R <broxsonmr@ornl.gov>
2019-08-08 11:40:34 -0400

    fixed issue with missing setter

--- src/components/VueBootstrapTypeahead.vue
+++ src/components/VueBootstrapTypeahead.vue
@@ -37,7 +37,11 @@
       @hit="handleHit"
     >
       <!-- pass down all scoped slots -->
-      <template v-for="(slot, slotName) in $scopedSlots" :slot="slotName" slot-scope="{ data, htmlText }">
+      <template
+        v-for="(slot, slotName) in $scopedSlots"
+        :slot="slotName"
+        slot-scope="{ data, htmlText }"
+      >
         <slot :name="slotName" v-bind="{ data, htmlText }"></slot>
       </template>
       <!-- below is the right solution, however if the user does not provide a scoped slot, vue will still set $scopedSlots.suggestion to a blank scope
@@ -49,11 +53,11 @@
 </template>
 
 <script>
-import VueBootstrapTypeaheadList from './VueBootstrapTypeaheadList.vue'
-import ResizeObserver from 'resize-observer-polyfill'
+import VueBootstrapTypeaheadList from "./VueBootstrapTypeaheadList.vue";
+import ResizeObserver from "resize-observer-polyfill";
 
 export default {
-  name: 'VueBootstrapTypehead',
+  name: "VueBootstrapTypehead",
 
   components: {
     VueBootstrapTypeaheadList
@@ -63,7 +67,7 @@ export default {
     size: {
       type: String,
       default: null,
-      validator: size => ['lg', 'sm'].indexOf(size) > -1
+      validator: size => ["lg", "sm"].indexOf(size) > -1
     },
     value: {
       type: String
@@ -75,14 +79,14 @@ export default {
     },
     serializer: {
       type: Function,
-      default: (d) => d,
+      default: d => d,
       validator: d => d instanceof Function
     },
     backgroundVariant: String,
     textVariant: String,
     inputClass: {
       type: String,
-      default: ''
+      default: ""
     },
     maxMatches: {
       type: Number,
@@ -99,68 +103,71 @@ export default {
 
   computed: {
     sizeClasses() {
-      return this.size ? `input-group input-group-${this.size}` : 'input-group'
+      return this.size ? `input-group input-group-${this.size}` : "input-group";
     },
 
     formattedData() {
       if (!(this.data instanceof Array)) {
-        return []
+        return [];
       }
       return this.data.map((d, i) => {
         return {
           id: i,
           data: d,
           text: this.serializer(d)
-        }
-      })
+        };
+      });
     },
 
-    inputValue: function() {
-      return this.value || ''
+    inputValue: {
+      get: function() {
+        return this.value || "";
+      },
+
+      set: function(newValue) {
+        this.inputValue = newValue;
+      }
     }
   },
 
   methods: {
     resizeList(el) {
-      const rect = el.getBoundingClientRect()
-      const listStyle = this.$refs.list.$el.style
+      const rect = el.getBoundingClientRect();
+      const listStyle = this.$refs.list.$el.style;
 
       // Set the width of the list on resize
-      listStyle.width = rect.width + 'px'
+      listStyle.width = rect.width + "px";
 
       // Set the margin when the prepend prop or slot is populated
       // (setting the "left" CSS property doesn't work)
       if (this.$refs.prependDiv) {
-        const prependRect = this.$refs.prependDiv.getBoundingClientRect()
-        listStyle.marginLeft = prependRect.width + 'px'
+        const prependRect = this.$refs.prependDiv.getBoundingClientRect();
+        listStyle.marginLeft = prependRect.width + "px";
       }
     },
 
     handleHit(evt) {
-      if (typeof this.value !== 'undefined') {
-        this.$emit('input', evt.text)
+      if (typeof this.value !== "undefined") {
+        this.$emit("input", evt.text);
       }
 
-      this.inputValue = evt.text
-      this.$emit('hit', evt.data)
-      this.$refs.input.blur()
-      this.isFocused = false
+      this.$emit("hit", evt.data);
+      this.$refs.input.blur();
+      this.isFocused = false;
     },
 
     handleBlur(evt) {
-      const tgt = evt.relatedTarget
-      if (tgt && tgt.classList.contains('vbst-item')) {
-        return
+      const tgt = evt.relatedTarget;
+      if (tgt && tgt.classList.contains("vbst-item")) {
+        return;
       }
-      this.isFocused = false
+      this.isFocused = false;
     },
 
     handleInput(newValue) {
-      this.inputValue = newValue
-
       // If v-model is being used, emit an input event
-      if (typeof this.value !== 'undefined') {
-        this.$emit('input', newValue)
+      if (typeof this.value !== "undefined") {
+        this.$emit("input", newValue);
       }
     }
   },
@@ -168,29 +175,29 @@ export default {
   data() {
     return {
       isFocused: false
-    }
+    };
   },
 
   mounted() {
     this.$_ro = new ResizeObserver(e => {
-      this.resizeList(this.$refs.input)
-    })
-    this.$_ro.observe(this.$refs.input)
-    this.$_ro.observe(this.$refs.list.$el)
+      this.resizeList(this.$refs.input);
+    });
+    this.$_ro.observe(this.$refs.input);
+    this.$_ro.observe(this.$refs.list.$el);
   },
 
   beforeDestroy() {
-    this.$_ro.disconnect()
+    this.$_ro.disconnect();
   }
-}
+};
 </script>
 
 <style scoped>
-  .vbt-autcomplete-list {
-    padding-top: 5px;
-    position: absolute;
-    max-height: 350px;
-    overflow-y: auto;
-    z-index: 999;
-  }
+.vbt-autcomplete-list {
+  padding-top: 5px;
+  position: absolute;
+  max-height: 350px;
+  overflow-y: auto;
+  z-index: 999;
+}
 </style>
