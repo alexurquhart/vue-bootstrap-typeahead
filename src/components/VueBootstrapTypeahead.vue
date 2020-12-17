83674bc82bdb2daf00b9b3151b51d9679a81398e
Gilson Doi Junior <doijunior@yahoo.com.br>
2020-12-16 22:50:12 -0300

    added sort-variant=length option

--- src/components/VueBootstrapTypeahead.vue
+++ src/components/VueBootstrapTypeahead.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div :class="sizeClasses">
-      <div ref="prependDiv" v-if="$slots.prepend || prepend" class="input-group-prepend">
+      <div
+        ref="prependDiv"
+        v-if="$slots.prepend || prepend"
+        class="input-group-prepend"
+      >
         <slot name="prepend">
           <span class="input-group-text">{{ prepend }}</span>
         </slot>
@@ -31,6 +35,7 @@
       :query="inputValue"
       :data="formattedData"
       :background-variant="backgroundVariant"
+      :sort-variant="sortVariant"
       :text-variant="textVariant"
       :maxMatches="maxMatches"
       :minMatchingChars="minMatchingChars"
@@ -38,7 +43,11 @@
       @hit="handleHit"
     >
       <!-- pass down all scoped slots -->
-      <template v-for="(slot, slotName) in $scopedSlots" :slot="slotName" slot-scope="{ data, htmlText }">
+      <template
+        v-for="(slot, slotName) in $scopedSlots"
+        :slot="slotName"
+        slot-scope="{ data, htmlText }"
+      >
         <slot :name="slotName" v-bind="{ data, htmlText }"></slot>
       </template>
       <!-- below is the right solution, however if the user does not provide a scoped slot, vue will still set $scopedSlots.suggestion to a blank scope
@@ -50,48 +59,51 @@
 </template>
 
 <script>
-import VueBootstrapTypeaheadList from './VueBootstrapTypeaheadList.vue'
-import ResizeObserver from 'resize-observer-polyfill'
+import VueBootstrapTypeaheadList from "./VueBootstrapTypeaheadList.vue";
+import ResizeObserver from "resize-observer-polyfill";
 
 export default {
-  name: 'VueBootstrapTypehead',
+  name: "VueBootstrapTypehead",
 
   components: {
-    VueBootstrapTypeaheadList
+    VueBootstrapTypeaheadList,
   },
 
   props: {
     size: {
       type: String,
       default: null,
-      validator: size => ['lg', 'sm'].indexOf(size) > -1
+      validator: (size) => ["lg", "sm"].indexOf(size) > -1,
     },
     value: {
-      type: String
+      type: String,
     },
     data: {
       type: Array,
       required: true,
-      validator: d => d instanceof Array
+      validator: (d) => d instanceof Array,
     },
     serializer: {
       type: Function,
       default: (d) => d,
-      validator: d => d instanceof Function
+      validator: (d) => d instanceof Function,
     },
     backgroundVariant: String,
+    sortVariant: {
+      type: String,
+    },
     textVariant: String,
     inputClass: {
       type: String,
-      default: ''
+      default: "",
     },
     maxMatches: {
       type: Number,
-      default: 10
+      default: 10,
     },
     minMatchingChars: {
       type: Number,
-      default: 2
+      default: 2,
     },
     removeDiacritcs: {
       type: Boolean,
@@ -99,100 +111,100 @@ export default {
     },
     placeholder: String,
     prepend: String,
-    append: String
+    append: String,
   },
 
   computed: {
     sizeClasses() {
-      return this.size ? `input-group input-group-${this.size}` : 'input-group'
+      return this.size ? `input-group input-group-${this.size}` : "input-group";
     },
 
     formattedData() {
       if (!(this.data instanceof Array)) {
-        return []
+        return [];
       }
       return this.data.map((d, i) => {
         return {
           id: i,
           data: d,
-          text: this.serializer(d)
-        }
-      })
-    }
+          text: this.serializer(d),
+        };
+      });
+    },
   },
 
   methods: {
     resizeList(el) {
-      const rect = el.getBoundingClientRect()
-      const listStyle = this.$refs.list.$el.style
+      const rect = el.getBoundingClientRect();
+      const listStyle = this.$refs.list.$el.style;
 
       // Set the width of the list on resize
-      listStyle.width = rect.width + 'px'
+      listStyle.width = rect.width + "px";
 
       // Set the margin when the prepend prop or slot is populated
       // (setting the "left" CSS property doesn't work)
       if (this.$refs.prependDiv) {
-        const prependRect = this.$refs.prependDiv.getBoundingClientRect()
-        listStyle.marginLeft = prependRect.width + 'px'
+        const prependRect = this.$refs.prependDiv.getBoundingClientRect();
+        listStyle.marginLeft = prependRect.width + "px";
       }
     },
 
     handleHit(evt) {
-      if (typeof this.value !== 'undefined') {
-        this.$emit('input', evt.text)
+      if (typeof this.value !== "undefined") {
+        this.$emit("input", evt.text);
       }
 
-      this.inputValue = evt.text
-      this.$emit('hit', evt.data)
-      this.$refs.input.blur()
-      this.isFocused = false
+      this.inputValue = evt.text;
+      this.$emit("hit", evt.data);
+      this.$refs.input.blur();
+      this.isFocused = false;
     },
 
     handleBlur(evt) {
-      const tgt = evt.relatedTarget
-      if (tgt && tgt.classList.contains('vbst-item')) {
-        return
+      const tgt = evt.relatedTarget;
+      if (tgt && tgt.classList.contains("vbst-item")) {
+        return;
       }
-      this.isFocused = false
+      this.isFocused = false;
     },
 
     handleInput(newValue) {
-      this.inputValue = newValue
+      this.inputValue = newValue;
 
       // If v-model is being used, emit an input event
-      if (typeof this.value !== 'undefined') {
-        this.$emit('input', newValue)
+      if (typeof this.value !== "undefined") {
+        this.$emit("input", newValue);
       }
-    }
+    },
   },
 
   data() {
     return {
       isFocused: false,
-      inputValue: ''
-    }
+      inputValue: "",
+    };
   },
 
   mounted() {
-    this.$_ro = new ResizeObserver(e => {
-      this.resizeList(this.$refs.input)
-    })
-    this.$_ro.observe(this.$refs.input)
-    this.$_ro.observe(this.$refs.list.$el)
+    this.$_ro = new ResizeObserver((e) => {
+      this.resizeList(this.$refs.input);
+    });
+    this.$_ro.observe(this.$refs.input);
+    this.$_ro.observe(this.$refs.list.$el);
   },
 
   beforeDestroy() {
-    this.$_ro.disconnect()
-  }
-}
+    this.$_ro.disconnect();
+  },
+};
 </script>
 
 <style scoped>
-  .vbt-autcomplete-list {
-    padding-top: 5px;
-    position: absolute;
-    max-height: 350px;
-    overflow-y: auto;
-    z-index: 999;
-  }
+.vbt-autcomplete-list {
+  padding-top: 5px;
+  position: absolute;
+  max-height: 350px;
+  overflow-y: auto;
+  z-index: 999;
+}
 </style>
--- src/components/VueBootstrapTypeaheadList.vue
+++ src/components/VueBootstrapTypeaheadList.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="list-group shadow">
     <vue-bootstrap-typeahead-list-item
-      v-for="(item, id) in matchedItems" :key="id"
+      v-for="(item, id) in matchedItems"
+      :key="id"
       :data="item.data"
       :html-text="highlight(item.text)"
       :background-variant="backgroundVariant"
       :text-variant="textVariant"
       @click.native="handleHit(item, $event)"
     >
-      <template v-if="$scopedSlots.suggestion" slot="suggestion" slot-scope="{ data, htmlText }">
+      <template
+        v-if="$scopedSlots.suggestion"
+        slot="suggestion"
+        slot-scope="{ data, htmlText }"
+      >
         <slot name="suggestion" v-bind="{ data, htmlText }" />
       </template>
     </vue-bootstrap-typeahead-list-item>
@@ -16,50 +21,53 @@
 </template>
 
 <script>
-import VueBootstrapTypeaheadListItem from './VueBootstrapTypeaheadListItem.vue'
+import VueBootstrapTypeaheadListItem from "./VueBootstrapTypeaheadListItem.vue";
 
 function normalize(text) {
-  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
 function sanitize(text) {
-  return text.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  return text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 }
 
 function escapeRegExp(str) {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
 export default {
-  name: 'VueBootstrapTypeaheadList',
+  name: "VueBootstrapTypeaheadList",
 
   components: {
-    VueBootstrapTypeaheadListItem
+    VueBootstrapTypeaheadListItem,
   },
 
   props: {
     data: {
       type: Array,
       required: true,
-      validator: d => d instanceof Array
+      validator: (d) => d instanceof Array,
     },
     query: {
       type: String,
-      default: ''
+      default: "",
     },
     backgroundVariant: {
-      type: String
+      type: String,
+    },
+    sortVariant: {
+      type: String,
     },
     textVariant: {
-      type: String
+      type: String,
     },
     maxMatches: {
       type: Number,
-      default: 10
+      default: 10,
     },
     minMatchingChars: {
       type: Number,
-      default: 2
+      default: 2,
     },
     removeDiacritcs: {
       type: Boolean,
@@ -69,49 +77,60 @@ export default {
   computed: {
     highlight() {
       return (text) => {
-        text = sanitize(text)
+        text = sanitize(text);
         if (this.query.length === 0) {
-          return text
+          return text;
         }
-        const re = new RegExp(this.escapedQuery, 'gi')
+        const re = new RegExp(this.escapedQuery, "gi");
 
-        return text.replace(re, `<strong>$&</strong>`)
-      }
+        return text.replace(re, `<strong>$&</strong>`);
+      };
     },
 
     escapedQuery() {
-      if(this.removeDiacritcs)
-        return escapeRegExp( normalize( sanitize(this.query) ) )
-      else
-        return escapeRegExp( sanitize(this.query) )
+      if (this.removeDiacritcs)
+        return escapeRegExp(normalize(sanitize(this.query)));
+      else return escapeRegExp(sanitize(this.query));
     },
 
     matchedItems() {
-      if (this.query.length === 0 || this.query.length < this.minMatchingChars) {
-        return []
+      if (
+        this.query.length === 0 ||
+        this.query.length < this.minMatchingChars
+      ) {
+        return [];
       }
 
-      const re = new RegExp(this.escapedQuery, 'gi')
+      const re = new RegExp(this.escapedQuery, "gi");
 
       // Filter, sort, and concat
       return this.data
-        .filter(i => i.text.match(re) !== null)
+        .filter((i) => i.text.match(re) !== null)
         .sort((a, b) => {
-          const aIndex = a.text.indexOf(a.text.match(re)[0])
-          const bIndex = b.text.indexOf(b.text.match(re)[0])
+          if (this.sortVariant === "lenght") {
+            return a.length - b.length;
+          } else {
+            const aIndex = a.text.indexOf(a.text.match(re)[0]);
+            const bIndex = b.text.indexOf(b.text.match(re)[0]);
 
-          if (aIndex < bIndex) { return -1 }
-          if (aIndex > bIndex) { return 1 }
-          return 0
-        }).slice(0, this.maxMatches)
-    }
+            if (aIndex < bIndex) {
+              return -1;
+            }
+            if (aIndex > bIndex) {
+              return 1;
+            }
+            return 0;
+          }
+        })
+        .slice(0, this.maxMatches);
+    },
   },
 
   methods: {
     handleHit(item, evt) {
-      this.$emit('hit', item)
-      evt.preventDefault()
-    }
-  }
-}
+      this.$emit("hit", item);
+      evt.preventDefault();
+    },
+  },
+};
 </script>
